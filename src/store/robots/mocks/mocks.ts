48d6c6556241a2ec555d543328952a87f253e679
Isaías Malvar Guíos <isaiasmg93@hotmail.com>
2023-05-13 13:15:53 +0200

    Add mocks for robots

---
+++ src/store/robots/mocks/mocks.ts
@@ -0,0 +1,20 @@
+import { RobotStructure } from "../../../types";
+
+export const robotTest: RobotStructure[] = [
+  {
+    creationDate: "15/05/1998",
+    id: 4,
+    image: "robot.jpg",
+    name: "Wall-E",
+    resistence: 100,
+    speed: 30,
+  },
+  {
+    creationDate: "15/05/1998",
+    id: 5,
+    image: "robot.jpg",
+    name: "EVA",
+    resistence: 100,
+    speed: 50,
+  },
+];
