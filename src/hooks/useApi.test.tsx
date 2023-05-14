755b684b40a07588a05a84edc2f3010be35c76e2
Judit Vives <jvivesramon@gmail.com>
2023-05-14 19:57:25 +0200

    Add use api and use api test

---
+++ src/hooks/useApi.test.tsx
@@ -0,0 +1,22 @@
+import { renderHook } from "@testing-library/react";
+import useApi from "./useApi";
+import { robotTest } from "../store/mocks/mocks";
+import { RobotStructure } from "../types";
+
+describe("Given a getRobots function,", () => {
+  describe("When it is called,", () => {
+    test("Then it should return a list of robots", async () => {
+      const expectedList: RobotStructure[] = robotTest;
+
+      const {
+        result: {
+          current: { getRobots },
+        },
+      } = renderHook(() => useApi());
+
+      const robots = await getRobots();
+
+      expect(robots).toStrictEqual(expectedList);
+    });
+  });
+});
---
+++ src/hooks/useApi.tsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { useCallback } from "react";
+import { RobotStructure } from "../types";
+const apiUrl = import.meta.env.VITE_API_URL;
+
+const useApi = () => {
+  const getRobots = useCallback(async (): Promise<RobotStructure[]> => {
+    const response = await axios.get(`${apiUrl}robots`);
+    const result = response.data;
+    return result;
+  }, []);
+
+  return { getRobots };
+};
+
+export default useApi;
